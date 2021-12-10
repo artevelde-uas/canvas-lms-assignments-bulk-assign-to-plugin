e3002a16d79fda746d695893e0ddd67879d44b4e
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-10 14:28:50 +0100

    Add package translations

--- src/index.js
+++ src/index.js
@@ -294,4 +294,10 @@ export default function () {
         };
 
     });
+
+    return {
+        ...require('../package.json'),
+        title: __('package.title'),
+        description: __('package.description')
+    };
 }
